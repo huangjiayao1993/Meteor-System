8b2755fcbdeee5e81cb8fc0572cbb0f0a94ce759
huangjiayao1993 <519471943@qq.com>
2023-07-24 17:40:56 +0800

    v1.3.0 1.新增动态系统配置 2.新增系统字典管理 3.修复系统操作日志部分参数保存异常 4.修改菜单排序异常

---
+++ src/views/system/dict/index.ts
@@ -0,0 +1,82 @@
+import {reactive} from "vue";
+import {BtnAuth} from "@/components/table/index";
+
+/**
+ * 按钮权限-登录日志
+ */
+class LoginBtnAuth extends BtnAuth {
+  constructor() {
+    super();
+    super.search = ['log:login:list']
+  }
+}
+export const loginAuth = reactive<LoginBtnAuth>(new LoginBtnAuth());
+
+/**
+ * 数据列-登录日志
+ */
+export const loginColumns: any = [
+  {
+    title: '账号',
+    dataIndex: 'username',
+  },
+  {
+    title: '登录方式',
+    dataIndex: 'loginType',
+  },
+  {
+    title: '时间',
+    dataIndex: 'createTime',
+  },
+  {
+    title: '登录状态',
+    slot: 'success',
+    dataIndex: 'success',
+  },
+  {
+    title: '失败原因',
+    dataIndex: 'reason',
+  },
+];
+
+/**
+ * 按钮权限-操作日志
+ */
+class operationBtnAuth extends BtnAuth {
+  constructor() {
+    super();
+    super.search = ['log:operation:list']
+  }
+}
+export const operationAuth = reactive<operationBtnAuth>(new operationBtnAuth());
+
+/**
+ * 数据列-操作日志
+ */
+export const operationColumns: any = [
+  {
+    title: '账号',
+    dataIndex: 'username',
+  },
+  {
+    title: '时间',
+    dataIndex: 'createTime',
+  },
+  {
+    title: 'IP地址',
+    dataIndex: 'ip',
+  },
+  {
+    title: '耗时',
+    dataIndex: 'duration',
+  },
+  {
+    title: '操作状态',
+    slot: 'success',
+    dataIndex: 'success',
+  },
+  {
+    title: '异常原因',
+    dataIndex: 'reason',
+  },
+];
